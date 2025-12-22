449a2d11f6ff7aadccb89dcce33c32d64d7fa005
Òscar Casajuana <elboletaire@gmail.com>
2025-12-22 10:31:32 +0100

    build(manifest): build farcaster manifest dynamically
    refs #48

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { farcasterManifestPlugin } from './vite/farcaster-manifest'
 
 // https://vitejs.dev/config/
 const viteconfig = defineConfig(({ mode }) => {
@@ -22,6 +23,7 @@ const viteconfig = defineConfig(({ mode }) => {
     },
     plugins: [
       react(),
+      farcasterManifestPlugin(),
       tsconfigPaths(),
       VitePWA({
         registerType: 'prompt',
